4b7a81da767d3f957bea91c4fcd7d54b9f23e20e
David87chdz <david87chdz@gmail.com>
2024-02-09 16:29:58 +0100

    Angular

--- Cliente/proyectoDragonAngular/src/app/app.component.spec.ts
+++ Cliente/proyectoDragonAngular/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'proyectoDragon' title`, () => {
+  it(`should have the 'proyectoDragonAngular' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('proyectoDragon');
+    expect(app.title).toEqual('proyectoDragonAngular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, proyectoDragon');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, proyectoDragonAngular');
   });
 });
