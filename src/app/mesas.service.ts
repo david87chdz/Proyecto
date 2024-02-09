772d7af1754f3dfad78f2254fe7b69432bc773af
david87chdz <david87chdz@gmail.com>
2024-02-09 10:08:07 +0100

    Angular

---
+++ src/app/mesas.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Mesa } from './mesa';
+import { HttpClient } from '@angular/common/http';
+// import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+@Injectable({
+  providedIn: 'root'
+})
+export class MesasService {
+
+  constructor(private http: HttpClient) { 
+
+  }
+
+  retornar(){
+    return this.http.get("http://127.0.0.1/")
+  }
+}
