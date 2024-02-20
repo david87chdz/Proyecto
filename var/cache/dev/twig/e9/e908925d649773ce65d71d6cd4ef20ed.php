aeea50e5a35705578f6099a5936084a55a44cac8
david87chdz <david87chdz@gmail.com>
2024-02-20 09:46:33 +0100

    asdasdda

---
+++ var/cache/dev/twig/e9/e908925d649773ce65d71d6cd4ef20ed.php
@@ -0,0 +1,245 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* mesa/index.html.twig */
+class __TwigTemplate_da4ca2864a8a95d3e2d2686f21d1df5f extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->blocks = [
+            'title' => [$this, 'block_title'],
+            'body' => [$this, 'block_body'],
+        ];
+    }
+
+    protected function doGetParent(array $context)
+    {
+        // line 1
+        return "base.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2 = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2->enter($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "mesa/index.html.twig"));
+
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "mesa/index.html.twig"));
+
+        $this->parent = $this->loadTemplate("base.html.twig", "mesa/index.html.twig", 1);
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+        
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2->leave($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof);
+
+        
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
+
+    }
+
+    // line 3
+    public function block_title($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2 = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2->enter($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "title"));
+
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "title"));
+
+        echo "Mesa index";
+        
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
+
+        
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2->leave($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof);
+
+    }
+
+    // line 5
+    public function block_body($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2 = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2->enter($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "body"));
+
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "body"));
+
+        // line 6
+        echo "    <h1>Mesa index</h1>
+
+    <table class=\"table\">
+        <thead>
+            <tr>
+                <th>Id</th>
+                <th>Disponible</th>
+                <th>actions</th>
+            </tr>
+        </thead>
+        <tbody>
+        ";
+        // line 17
+        $context['_parent'] = $context;
+        $context['_seq'] = twig_ensure_traversable((isset($context["mesas"]) || array_key_exists("mesas", $context) ? $context["mesas"] : (function () { throw new RuntimeError('Variable "mesas" does not exist.', 17, $this->source); })()));
+        $context['_iterated'] = false;
+        foreach ($context['_seq'] as $context["_key"] => $context["mesa"]) {
+            // line 18
+            echo "            <tr>
+                <td>";
+            // line 19
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["mesa"], "id", [], "any", false, false, false, 19), "html", null, true);
+            echo "</td>
+                <td>";
+            // line 20
+            echo ((twig_get_attribute($this->env, $this->source, $context["mesa"], "disponible", [], "any", false, false, false, 20)) ? ("Yes") : ("No"));
+            echo "</td>
+                <td>
+                    <a href=\"";
+            // line 22
+            echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("app_mesa_show", ["id" => twig_get_attribute($this->env, $this->source, $context["mesa"], "id", [], "any", false, false, false, 22)]), "html", null, true);
+            echo "\">show</a>
+                    <a href=\"";
+            // line 23
+            echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("app_mesa_edit", ["id" => twig_get_attribute($this->env, $this->source, $context["mesa"], "id", [], "any", false, false, false, 23)]), "html", null, true);
+            echo "\">edit</a>
+                </td>
+            </tr>
+        ";
+            $context['_iterated'] = true;
+        }
+        if (!$context['_iterated']) {
+            // line 27
+            echo "            <tr>
+                <td colspan=\"3\">no records found</td>
+            </tr>
+        ";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['_key'], $context['mesa'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+        // line 31
+        echo "        </tbody>
+    </table>
+
+    <h3>Mesas por tamaño</h3>
+    <ul>
+    ";
+        // line 36
+        $context['_parent'] = $context;
+        $context['_seq'] = twig_ensure_traversable((isset($context["mesatam"]) || array_key_exists("mesatam", $context) ? $context["mesatam"] : (function () { throw new RuntimeError('Variable "mesatam" does not exist.', 36, $this->source); })()));
+        foreach ($context['_seq'] as $context["_key"] => $context["mesa"]) {
+            // line 37
+            echo "        <li>";
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["mesa"], "id", [], "any", false, false, false, 37), "html", null, true);
+            echo "</td>
+    ";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['_key'], $context['mesa'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+        // line 39
+        echo "    </ul>
+    
+    <a href=\"";
+        // line 41
+        echo $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("app_mesa_new");
+        echo "\">Create new</a>
+";
+        
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
+
+        
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2->leave($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof);
+
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getTemplateName()
+    {
+        return "mesa/index.html.twig";
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getDebugInfo()
+    {
+        return array (  164 => 41,  160 => 39,  151 => 37,  147 => 36,  140 => 31,  131 => 27,  122 => 23,  118 => 22,  113 => 20,  109 => 19,  106 => 18,  101 => 17,  88 => 6,  78 => 5,  59 => 3,  36 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("{% extends 'base.html.twig' %}
+
+{% block title %}Mesa index{% endblock %}
+
+{% block body %}
+    <h1>Mesa index</h1>
+
+    <table class=\"table\">
+        <thead>
+            <tr>
+                <th>Id</th>
+                <th>Disponible</th>
+                <th>actions</th>
+            </tr>
+        </thead>
+        <tbody>
+        {% for mesa in mesas %}
+            <tr>
+                <td>{{ mesa.id }}</td>
+                <td>{{ mesa.disponible ? 'Yes' : 'No' }}</td>
+                <td>
+                    <a href=\"{{ path('app_mesa_show', {'id': mesa.id}) }}\">show</a>
+                    <a href=\"{{ path('app_mesa_edit', {'id': mesa.id}) }}\">edit</a>
+                </td>
+            </tr>
+        {% else %}
+            <tr>
+                <td colspan=\"3\">no records found</td>
+            </tr>
+        {% endfor %}
+        </tbody>
+    </table>
+
+    <h3>Mesas por tamaño</h3>
+    <ul>
+    {% for mesa in mesatam %}
+        <li>{{ mesa.id}}</td>
+    {% endfor %}
+    </ul>
+    
+    <a href=\"{{ path('app_mesa_new') }}\">Create new</a>
+{% endblock %}
+", "mesa/index.html.twig", "C:\\Users\\Alumno\\Desktop\\proyectoDragon\\proyectoDragon\\templates\\mesa\\index.html.twig");
+    }
+}
---
+++ var/cache/dev/url_matching_routes.php
@@ -0,0 +1,118 @@
+<?php
+
+/**
+ * This file has been auto-generated
+ * by the Symfony Routing Component.
+ */
+
+return [
+    false, // $matchHost
+    [ // $staticRoutes
+        '/_profiler' => [[['_route' => '_profiler_home', '_controller' => 'web_profiler.controller.profiler::homeAction'], null, null, null, true, false, null]],
+        '/_profiler/search' => [[['_route' => '_profiler_search', '_controller' => 'web_profiler.controller.profiler::searchAction'], null, null, null, false, false, null]],
+        '/_profiler/search_bar' => [[['_route' => '_profiler_search_bar', '_controller' => 'web_profiler.controller.profiler::searchBarAction'], null, null, null, false, false, null]],
+        '/_profiler/phpinfo' => [[['_route' => '_profiler_phpinfo', '_controller' => 'web_profiler.controller.profiler::phpinfoAction'], null, null, null, false, false, null]],
+        '/_profiler/xdebug' => [[['_route' => '_profiler_xdebug', '_controller' => 'web_profiler.controller.profiler::xdebugAction'], null, null, null, false, false, null]],
+        '/_profiler/open' => [[['_route' => '_profiler_open_file', '_controller' => 'web_profiler.controller.profiler::openAction'], null, null, null, false, false, null]],
+        '/juego' => [[['_route' => 'app_juego_index', '_controller' => 'App\\Controller\\JuegoController::index'], null, ['GET' => 0], null, true, false, null]],
+        '/juego/insertarJuego' => [[['_route' => 'insertarJuego', '_controller' => 'App\\Controller\\JuegoController::insertarJuego'], null, ['POST' => 0], null, false, false, null]],
+        '/juego/getJuegos' => [[['_route' => 'getJuegos', '_controller' => 'App\\Controller\\JuegoController::getTodas'], null, ['GET' => 0], null, false, false, null]],
+        '/juego/new' => [[['_route' => 'app_juego_new', '_controller' => 'App\\Controller\\JuegoController::new'], null, ['GET' => 0, 'POST' => 1], null, false, false, null]],
+        '/mesa' => [[['_route' => 'app_mesa_index', '_controller' => 'App\\Controller\\MesaController::index'], null, ['GET' => 0], null, true, false, null]],
+        '/mesa/new' => [[['_route' => 'app_mesa_new', '_controller' => 'App\\Controller\\MesaController::new'], null, ['GET' => 0, 'POST' => 1], null, false, false, null]],
+        '/reserva' => [[['_route' => 'app_reserva_index', '_controller' => 'App\\Controller\\ReservaController::index'], null, ['GET' => 0], null, true, false, null]],
+        '/reserva/new' => [[['_route' => 'app_reserva_new', '_controller' => 'App\\Controller\\ReservaController::new'], null, ['GET' => 0, 'POST' => 1], null, false, false, null]],
+        '/rol' => [[['_route' => 'app_rol_index', '_controller' => 'App\\Controller\\RolController::index'], null, ['GET' => 0], null, true, false, null]],
+        '/rol/new' => [[['_route' => 'app_rol_new', '_controller' => 'App\\Controller\\RolController::new'], null, ['GET' => 0, 'POST' => 1], null, false, false, null]],
+        '/tipo/mesa' => [[['_route' => 'app_tipo_mesa_index', '_controller' => 'App\\Controller\\TipoMesaController::index'], null, ['GET' => 0], null, true, false, null]],
+        '/tipo/mesa/new' => [[['_route' => 'app_tipo_mesa_new', '_controller' => 'App\\Controller\\TipoMesaController::new'], null, ['GET' => 0, 'POST' => 1], null, false, false, null]],
+        '/usuario' => [[['_route' => 'app_usuario_index', '_controller' => 'App\\Controller\\UsuarioController::index'], null, ['GET' => 0], null, true, false, null]],
+        '/usuario/new' => [[['_route' => 'app_usuario_new', '_controller' => 'App\\Controller\\UsuarioController::new'], null, ['GET' => 0, 'POST' => 1], null, false, false, null]],
+    ],
+    [ // $regexpList
+        0 => '{^(?'
+                .'|/_(?'
+                    .'|error/(\\d+)(?:\\.([^/]++))?(*:38)'
+                    .'|wdt/([^/]++)(*:57)'
+                    .'|profiler/(?'
+                        .'|font/([^/\\.]++)\\.woff2(*:98)'
+                        .'|([^/]++)(?'
+                            .'|/(?'
+                                .'|search/results(*:134)'
+                                .'|router(*:148)'
+                                .'|exception(?'
+                                    .'|(*:168)'
+                                    .'|\\.css(*:181)'
+                                .')'
+                            .')'
+                            .'|(*:191)'
+                        .')'
+                    .')'
+                .')'
+                .'|/juego/([^/]++)(?'
+                    .'|(*:220)'
+                    .'|/edit(*:233)'
+                    .'|(*:241)'
+                .')'
+                .'|/mesa/([^/]++)(?'
+                    .'|(*:267)'
+                    .'|/edit(*:280)'
+                    .'|(*:288)'
+                .')'
+                .'|/r(?'
+                    .'|eserva/([^/]++)(?'
+                        .'|(*:320)'
+                        .'|/edit(*:333)'
+                        .'|(*:341)'
+                    .')'
+                    .'|ol/([^/]++)(?'
+                        .'|(*:364)'
+                        .'|/edit(*:377)'
+                        .'|(*:385)'
+                    .')'
+                .')'
+                .'|/tipo/mesa/([^/]++)(?'
+                    .'|(*:417)'
+                    .'|/edit(*:430)'
+                    .'|(*:438)'
+                .')'
+                .'|/usuario/([^/]++)(?'
+                    .'|(*:467)'
+                    .'|/edit(*:480)'
+                    .'|(*:488)'
+                .')'
+            .')/?$}sDu',
+    ],
+    [ // $dynamicRoutes
+        38 => [[['_route' => '_preview_error', '_controller' => 'error_controller::preview', '_format' => 'html'], ['code', '_format'], null, null, false, true, null]],
+        57 => [[['_route' => '_wdt', '_controller' => 'web_profiler.controller.profiler::toolbarAction'], ['token'], null, null, false, true, null]],
+        98 => [[['_route' => '_profiler_font', '_controller' => 'web_profiler.controller.profiler::fontAction'], ['fontName'], null, null, false, false, null]],
+        134 => [[['_route' => '_profiler_search_results', '_controller' => 'web_profiler.controller.profiler::searchResultsAction'], ['token'], null, null, false, false, null]],
+        148 => [[['_route' => '_profiler_router', '_controller' => 'web_profiler.controller.router::panelAction'], ['token'], null, null, false, false, null]],
+        168 => [[['_route' => '_profiler_exception', '_controller' => 'web_profiler.controller.exception_panel::body'], ['token'], null, null, false, false, null]],
+        181 => [[['_route' => '_profiler_exception_css', '_controller' => 'web_profiler.controller.exception_panel::stylesheet'], ['token'], null, null, false, false, null]],
+        191 => [[['_route' => '_profiler', '_controller' => 'web_profiler.controller.profiler::panelAction'], ['token'], null, null, false, true, null]],
+        220 => [[['_route' => 'app_juego_show', '_controller' => 'App\\Controller\\JuegoController::show'], ['id'], ['GET' => 0], null, false, true, null]],
+        233 => [[['_route' => 'app_juego_edit', '_controller' => 'App\\Controller\\JuegoController::edit'], ['id'], ['GET' => 0, 'POST' => 1], null, false, false, null]],
+        241 => [[['_route' => 'app_juego_delete', '_controller' => 'App\\Controller\\JuegoController::delete'], ['id'], ['POST' => 0], null, false, true, null]],
+        267 => [[['_route' => 'app_mesa_show', '_controller' => 'App\\Controller\\MesaController::show'], ['id'], ['GET' => 0], null, false, true, null]],
+        280 => [[['_route' => 'app_mesa_edit', '_controller' => 'App\\Controller\\MesaController::edit'], ['id'], ['GET' => 0, 'POST' => 1], null, false, false, null]],
+        288 => [[['_route' => 'app_mesa_delete', '_controller' => 'App\\Controller\\MesaController::delete'], ['id'], ['POST' => 0], null, false, true, null]],
+        320 => [[['_route' => 'app_reserva_show', '_controller' => 'App\\Controller\\ReservaController::show'], ['id'], ['GET' => 0], null, false, true, null]],
+        333 => [[['_route' => 'app_reserva_edit', '_controller' => 'App\\Controller\\ReservaController::edit'], ['id'], ['GET' => 0, 'POST' => 1], null, false, false, null]],
+        341 => [[['_route' => 'app_reserva_delete', '_controller' => 'App\\Controller\\ReservaController::delete'], ['id'], ['POST' => 0], null, false, true, null]],
+        364 => [[['_route' => 'app_rol_show', '_controller' => 'App\\Controller\\RolController::show'], ['id'], ['GET' => 0], null, false, true, null]],
+        377 => [[['_route' => 'app_rol_edit', '_controller' => 'App\\Controller\\RolController::edit'], ['id'], ['GET' => 0, 'POST' => 1], null, false, false, null]],
+        385 => [[['_route' => 'app_rol_delete', '_controller' => 'App\\Controller\\RolController::delete'], ['id'], ['POST' => 0], null, false, true, null]],
+        417 => [[['_route' => 'app_tipo_mesa_show', '_controller' => 'App\\Controller\\TipoMesaController::show'], ['id'], ['GET' => 0], null, false, true, null]],
+        430 => [[['_route' => 'app_tipo_mesa_edit', '_controller' => 'App\\Controller\\TipoMesaController::edit'], ['id'], ['GET' => 0, 'POST' => 1], null, false, false, null]],
+        438 => [[['_route' => 'app_tipo_mesa_delete', '_controller' => 'App\\Controller\\TipoMesaController::delete'], ['id'], ['POST' => 0], null, false, true, null]],
+        467 => [[['_route' => 'app_usuario_show', '_controller' => 'App\\Controller\\UsuarioController::show'], ['id'], ['GET' => 0], null, false, true, null]],
+        480 => [[['_route' => 'app_usuario_edit', '_controller' => 'App\\Controller\\UsuarioController::edit'], ['id'], ['GET' => 0, 'POST' => 1], null, false, false, null]],
+        488 => [
+            [['_route' => 'app_usuario_delete', '_controller' => 'App\\Controller\\UsuarioController::delete'], ['id'], ['POST' => 0], null, false, true, null],
+            [null, null, null, null, false, false, 0],
+        ],
+    ],
+    null, // $checkCondition
+];
