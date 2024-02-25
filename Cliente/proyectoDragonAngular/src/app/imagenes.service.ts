6bdd963deca15ac5d6a2c3a65e046a01ca03b092
David87chdz <david87chdz@gmail.com>
2024-02-25 23:26:13 +0100

    Ultimos Cambios

--- Cliente/proyectoDragonAngular/src/app/imagenes.service.ts
+++ Cliente/proyectoDragonAngular/src/app/imagenes.service.ts
@@ -8,10 +8,10 @@ export class ImagenesService {
   constructor() { }
   getImagenes() {
     return [
-      { id: 1, nombre:'ajedre', imagen: 'assets/images/chess.jpg' },
+      { id: 1, nombre:'ajedre', imagen: 'http://localhost:4200/assets/images/continuar.png' },
       { id: 2, nombre:'dragon', imagen: 'http://localhost:4200/assets/images/continuar.png' },
-      { id: 3, nombre:'monopolio', imagen: 'http://localhost:4200/assets/images/monopoly.jpg' },
-      { id: 4,  nombre:'parchis',imagen: '../../../../assets/images/parchis.png' }
+      { id: 3, nombre:'monopolio', imagen: 'http://localhost:4200/assets/images/continuar.png' },
+      { id: 4,  nombre:'parchis',imagen: 'http://localhost:4200/assets/images/continuar.png' }
     ];
   }
 }
